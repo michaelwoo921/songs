1158074ef46f90c817cf2acb8e2d7895e7437836
Michael Woo <michaelwoo921@gmail.com>
2020-04-24 15:34:46 -0700

    action creators and reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import SongList from './SongList';
 import SongDetail from './SongDetail';
+import {selectSong} from '../actions'
 
-class App extends React.Component {
-    state ={ songs: 
-        [
-            { title: 'No Scrubs', duration: '4:05' },
-            { title: 'Macarena', duration: '2:30' },
-            { title: 'All Star', duration: '3:15' },
-            { title: 'I Want it That Way', duration: '1:45' }
-        ] ,
-        selectedSong: null
-    };
+const App =()=> {
 
-    render(){
         return (
             <div className="ui container grid"> 
                 <div className="ui row">
@@ -26,7 +17,7 @@ class App extends React.Component {
                 </div>                                       
             </div>
         );
-    }
+    
 }
    
 
